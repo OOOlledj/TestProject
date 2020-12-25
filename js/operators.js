9a1a1bee6ba89d4ba56a9bb1b1e13602d394dd52
Oleg <IPHON_g5@mail.ru>
2020-12-25 23:25:43 +0300

    first commit

---
+++ js/operators.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+console.log(4 + +"string");
+
+let inc = 10,
+    dec = 10;
+/*
+console.log(inc++);
+console.log(dec--);*/
+
+console.log(++inc); 
+console.log(--dec);
+
+console.log(2*4 == '8'); //true
+console.log(2*4 === '8'); //false
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
